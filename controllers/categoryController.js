cba0966aa57333a91b8b622de92d4db33e97195f
Yao <capslock0921@gmail.com>
2020-09-15 21:54:14 +0800

    add admin categories edit and put feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,9 +7,20 @@ let categoryController = {
       nest: true
     })
       .then(categories => {
-        return res.render('admin/categories', { categories })
+        if (req.params.id) {
+          Category.findByPk(req.params.id)
+            .then((category) => {
+              return res.render('admin/categories', {
+                categories,
+                category: category.toJSON()
+              })
+            })
+        } else {
+          return res.render('admin/categories', { categories })
+        }
       })
   },
+
   postCategories: (req, res) => {
     if (!req.body.name) {
       req.flash('error_messages', '請輸入分類名稱。')
@@ -20,6 +31,21 @@ let categoryController = {
       })
         .then(categories => {
           res.redirect('/admin/categories')
+        })
+    }
+  },
+
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '請輸入分類名稱。')
+      return res.redirect('back')
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              res.redirect('/admin/categories')
+            })
         })
     }
   }
