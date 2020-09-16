3f6d003cde6bcf950d1f31e6b8aa42814dd0f906
Yao <capslock0921@gmail.com>
2020-09-16 20:10:03 +0800

    modify migrations

--- migrations/20200909132333-add-isAdmin-to-users.js
+++ migrations/20200909132333-add-isAdmin-to-users.js
@@ -4,9 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'isAdmin', {
       type: Sequelize.BOOLEAN,
-      defaultValue: false,
-    });
-
+      defaultValue: false
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
