654ce68752d1732eca63f783f0309c44b9eae482
Yao <capslock0921@gmail.com>
2020-09-16 17:54:00 +0800

    add user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
 const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
@@ -42,6 +45,62 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    if (req.user.id !== Number(req.params.id)) {
+      req.flash('error_messages', '僅可查詢個人資料頁面。')
+      return res.redirect('back')
+    }
+
+    User.findByPk(req.params.id)
+      .then(user => {
+        res.render('users', { user: user.toJSON() })
+      })
+  },
+
+  editUser: (req, res) => {
+    User.findByPk(req.params.id)
+      .then(user => {
+        res.render('editUser')
+      })
+  },
+
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '請輸入使用者姓名')
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then(user => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : user.image
+            })
+              .then(user => {
+                req.flash('success_messages', '已成功更新個人資料。')
+                res.redirect(`/users/${user.id}`)
+              })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then(user => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          })
+            .then(user => {
+              req.flash('success_messages', '已成功更新個人資料。')
+              res.redirect(`/users/${user.id}`)
+            })
+        })
+    }
   }
 }
 
