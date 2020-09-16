c322d720f306dbe367e4bac7d253fe9b32cbb763
Yao <capslock0921@gmail.com>
2020-09-16 11:24:46 +0800

    add post comment feature on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 const categoryController = require('../controllers/categoryController')
+const commentController = require('../controllers/commentController')
+
 const user = require('../models/user')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -33,16 +35,11 @@ module.exports = (app, passport) => {
     res.redirect('/admin/restaurants')
   })
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
-  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant
-  )
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
-
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
-
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
-
   app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
-
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
@@ -50,10 +47,8 @@ module.exports = (app, passport) => {
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
-
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-
   app.get('/logout', userController.logout)
 
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
@@ -61,4 +56,6 @@ module.exports = (app, passport) => {
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
+
+  app.post('/comments', authenticated, commentController.postComment)
 }
