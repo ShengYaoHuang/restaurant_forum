5a990c8e5a63219dd9bd2f50fb248e35adc51912
Yao <capslock0921@gmail.com>
2020-09-16 22:25:14 +0800

    add feeds page and function

--- controllers/restController.js
+++ controllers/restController.js
@@ -62,6 +62,30 @@ const restController = {
       .then(restaurant => {
         return res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
+  },
+
+  getFeeds: (req, res) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      limit: 10,
+      order: [['createdAt', 'DESC']],
+      include: [Category]
+    })
+      .then(restaurants => {
+        Comment.findAll({
+          raw: true,
+          nest: true,
+          limit: 10,
+          order: [['createdAt', 'DESC']],
+          include: [User, Restaurant]
+        })
+          .then(comments => {
+            return res.render('feeds', {
+              restaurants, comments
+            })
+          })
+      })
   }
 }
 
