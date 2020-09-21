c909610293da1a0661f295076ded3081711bdcd4
Yao <capslock0921@gmail.com>
2020-09-21 20:51:00 +0800

    refactor adminController.deleteRestaurant

--- services/adminService.js
+++ services/adminService.js
@@ -24,6 +24,17 @@ const adminService = {
       })
   },
 
+  deleteRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        restaurant.destroy()
+          .then(restaurant => {
+            callback({ status: 'success', message: '' })
+          })
+      })
+  },
+
+
   getCategories: (req, res, callback) => {
     return Category.findAll({
       raw: true,
